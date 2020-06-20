737fb2b866d43219f46592c8876da60eed88561f
Fernando74lr <flopezramirez@hotmail.com>
2020-06-19 23:38:32 -0500

    Simple random selector of movies choices

--- Choose movie/scripts.js
+++ Choose movie/scripts.js
@@ -13,25 +13,23 @@ const Fernando = ['Falsa identidad', 'Baywatch'];
 const Kaleb = ['Ya no estoy aquí', 'NULL Unu jajaja'];
 
 const options = [Nicolas, Benjamin, Fernanda, Max, Edgar, Fernando, Kaleb];
+
 function boogie() {
-let random_winner = Math.floor(Math.random() * 6);
-let random_movie = Math.round(Math.random());
-console.log(random_winner);
-console.log(random_movie);
+    let random_winner = Math.floor(Math.random() * 6);
+    let random_movie = Math.round(Math.random());
+    let final_winner;
 
-let final_winner;
-switch(random_winner) {
-    case 0: final_winner = 'Nico'; break;
-    case 1: final_winner = 'Benjamín'; break;
-    case 2: final_winner = 'Fer mujer'; break;
-    case 3: final_winner = 'Max'; break;
-    case 4: final_winner = 'Edgar'; break;
-    case 5: final_winner = 'Fer hombre'; break;
-    case 6: final_winner = 'Kaleb'; break;
-}
+    switch(random_winner) {
+        case 0: final_winner = 'Nico'; break;
+        case 1: final_winner = 'Benjamín'; break;
+        case 2: final_winner = 'Fer mujer'; break;
+        case 3: final_winner = 'Max'; break;
+        case 4: final_winner = 'Edgar'; break;
+        case 5: final_winner = 'Fer hombre'; break;
+        case 6: final_winner = 'Kaleb'; break;
+    }
 
-let final_movie = options[random_winner][random_movie];
-
+    let final_movie = options[random_winner][random_movie];
 
     document.getElementById("name_winner").innerHTML = "Winner: " + final_winner;
     document.getElementById("name_movie").innerHTML = "Movie: " + final_movie;
