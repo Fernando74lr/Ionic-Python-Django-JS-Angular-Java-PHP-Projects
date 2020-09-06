b0249fc8ca97e2a9a956f8daa19f7ae7bda41110
Fernando <flopezramirez@hotmail.com>
2020-09-05 21:50:11 -0500

    DateTime

--- Ionic/03-componentes/src/app/pages/date-time/date-time.page.ts
+++ Ionic/03-componentes/src/app/pages/date-time/date-time.page.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DateTimePage implements OnInit {
 
+  birthdate: Date = new Date();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeDate(event) {
+    console.log('ionChange', event.detail);
+    console.log('Date', new Date(event.detail.value));
   }
 
 }
