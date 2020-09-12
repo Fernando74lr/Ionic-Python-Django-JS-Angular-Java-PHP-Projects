80f985cc58080f775b7307f170dd4ab169979056
Fernando <flopezramirez@hotmail.com>
2020-09-11 23:00:57 -0500

    List Item Sliding

--- Ionic/03-componentes/src/app/pages/list/list.page.ts
+++ Ionic/03-componentes/src/app/pages/list/list.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -9,12 +10,29 @@ import { Observable } from 'rxjs';
 })
 export class ListPage implements OnInit {
 
+  @ViewChild('lista') lista: IonList;
+
   usuarios: Observable<any>;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
+  }
+
+  favorite(user) {
+    console.log(user);
+    this.lista.closeSlidingItems();
+  }
+
+  share(user) {
+    console.log(user);
+    this.lista.closeSlidingItems();
+  }
+
+  borrar(user) {
+    console.log(user);
+    this.lista.closeSlidingItems();
   }
 
 }
