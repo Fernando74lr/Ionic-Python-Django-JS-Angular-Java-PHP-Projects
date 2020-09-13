f184bfe0c1c6a56b1dcd744751000fc74717d71f
Fernando <flopezramirez@hotmail.com>
2020-09-12 23:58:04 -0500

    Menu

--- Ionic/03-componentes/src/app/components/components.module.ts
+++ Ionic/03-componentes/src/app/components/components.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule,
