482898a95db16f11aa6742c71a11e2462e357302
Fernando <flopezramirez@hotmail.com>
2020-09-17 07:37:48 -0500

    Seach Bar with Filter (pipe)

--- Ionic/03-componentes/src/app/pages/search/search.page.ts
+++ Ionic/03-componentes/src/app/pages/search/search.page.ts
@@ -9,6 +9,7 @@ import { DataService } from '../../services/data.service';
 export class SearchPage implements OnInit {
 
   albumes: any[] = [];
+  textoBuscar: string = '';
 
   constructor(private dataService: DataService) { }
 
@@ -20,7 +21,7 @@ export class SearchPage implements OnInit {
   }
 
   onSearchChange(event: any) {
-    console.log(event);
+    this.textoBuscar = event.detail.value;
   }
 
 }
