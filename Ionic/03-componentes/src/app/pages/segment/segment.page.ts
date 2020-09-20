3ca017eb596500ae3eea41741e58d607a462caa7
Fernando <flopezramirez@hotmail.com>
2020-09-19 22:50:25 -0500

    Segment with Filter

--- Ionic/03-componentes/src/app/pages/segment/segment.page.ts
+++ Ionic/03-componentes/src/app/pages/segment/segment.page.ts
@@ -19,10 +19,6 @@ export class SegmentPage implements OnInit {
   }
 
   segmentChanged(event) {
-    console.log(event.detail.value);
+    this.company = event.detail.value;
   }
-
-  getDc = () => this.company = "DC";
-
-  getMarvel = () => this.company = "Marvel";
 }
