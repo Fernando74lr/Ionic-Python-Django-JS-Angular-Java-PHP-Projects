9f9485a7e07485068b4f17b3e54d6f34af137f35
Fernando <flopezramirez@hotmail.com>
2020-09-23 12:09:36 -0500

    Show Headlines Depending On The Category

--- Ionic/04-news/src/app/pages/tab2/tab2.page.ts
+++ Ionic/04-news/src/app/pages/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
+import { Article } from 'src/app/interfaces/interfaces';
 import { NewsService } from 'src/app/services/news.service';
 
 @Component({
@@ -12,19 +13,34 @@ export class Tab2Page implements OnInit {
   @ViewChild(IonSegment) segment: IonSegment;
 
   categories = ["business", "entertainment", "general", "health", "science", "sports", "technology"];
+  news: Article[]= [];
 
-  constructor(private newsService: NewsService) {
-
-  }
+  constructor(private newsService: NewsService) { }
 
   ngOnInit() {
-    this.newsService.getTopHeadlinesCategory(this.categories[0])
-    .subscribe(data => console.log(data));
+    // Get the top headlines.
+    this.loadNews(this.categories[0]);
   }
 
   ionViewDidEnter() {
+    // Set the first category as selected.
     this.segment.value = this.categories[0];
   }
 
+  categoryChange(event) {
+    // Reset the array.
+    this.news = [];
+    // Show news depending on the category.
+    this.loadNews(event.detail.value); 
+  }
+
+  loadNews(category: string) {
+    // Get the top headlines from the API using our service.
+    this.newsService.getTopHeadlinesCategory(category)
+      .subscribe(data => {
+        console.log(data);
+        this.news.push(...data.articles)
+      });
+  }
 
 }
