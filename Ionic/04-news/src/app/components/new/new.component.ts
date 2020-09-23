c5ad5520e5ac81a06264c3056c50db0ff4291d9c
Fernando <flopezramirez@hotmail.com>
2020-09-23 13:59:35 -0500

    Open Website of New in the Browser

--- Ionic/04-news/src/app/components/new/new.component.ts
+++ Ionic/04-news/src/app/components/new/new.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-new',
@@ -11,9 +12,13 @@ export class NewComponent implements OnInit {
   @Input() new: Article;
   @Input() index: number;
 
-  constructor() { }
+  constructor(private iab: InAppBrowser) { }
 
   ngOnInit() {}
 
+  openLink() {
+    console.log(this.new.url);
+    const browser = this.iab.create(this.new.url, '_system');
+  }
 
 }
