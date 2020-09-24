98bae9ff5c8a0f0577a4de16c950c2fe10be177d
Fernando <flopezramirez@hotmail.com>
2020-09-24 09:46:34 -0500

    Show Action-Sheet In The News

--- Ionic/04-news/src/app/components/new/new.component.ts
+++ Ionic/04-news/src/app/components/new/new.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-new',
@@ -12,13 +13,43 @@ export class NewComponent implements OnInit {
   @Input() new: Article;
   @Input() index: number;
 
-  constructor(private iab: InAppBrowser) { }
+  constructor(private iab: InAppBrowser,
+              public actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {}
 
   openLink() {
     console.log(this.new.url);
     const browser = this.iab.create(this.new.url, '_system');
+  }
+
+  async openMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [{
+        text: 'Share',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'star',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
