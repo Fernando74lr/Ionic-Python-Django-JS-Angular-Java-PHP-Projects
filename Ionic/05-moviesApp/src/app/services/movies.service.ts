e6ab220104522c8f3f4ed868a0ed6ba63893bd79
Fernando74lr <flopezramirez@hotmail.com>
2020-10-04 08:06:03 -0500

    Service To Get Movies From API

---
+++ Ionic/05-moviesApp/src/app/services/movies.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(private http: HttpClient) { }
+
+  getFeature() {
+    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2020-10-01&api_key=112f7f9a47c2cd93b18968c51ed2d65e`);
+  }
+}
