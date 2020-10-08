190defab1f187786f635decb71efce5b300edf29
unknown <flopezramirez@hotmail.com>
2020-10-08 15:55:11 -0500

    Design Of Search Movies Tab pt.2

--- Ionic/05-moviesApp/src/app/tab2/tab2.page.ts
+++ Ionic/05-moviesApp/src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { DetailComponent } from '../components/detail/detail.component';
+import { Movie } from '../interfaces/interfaces';
 import { MoviesService } from '../services/movies.service';
 
 @Component({
@@ -8,17 +11,49 @@ import { MoviesService } from '../services/movies.service';
 })
 export class Tab2Page {
 
-  ideas: string[] = ['Spider-Man', 'Sherlock Holmes', 'The Shining', 'Avengers', 'Lord Of The Rings', 'Life Itself'];
+  ideas: string[] = [
+    'Spider-Man',
+    'Sherlock Holmes',
+    'The Shining',
+    'Avengers',
+    'Lord Of The Rings',
+    'Life Itself'
+  ];
+  movies: Movie[] = [];
   searchText = '';
+  searching: Boolean = false;
 
-  constructor(private moviesService: MoviesService) {}
+  constructor(private moviesService: MoviesService,
+              private modalCtrl: ModalController) {}
+
+  
+  async showDetail(id: string) {
+    const modal = await this.modalCtrl.create({
+      component: DetailComponent,
+      componentProps: {
+        id
+      }
+    });
+
+    modal.present();
+  }
 
   search(event) {
     const movie = event.detail.value;
+
+    if (movie.length === 0) {
+      this.searching = false;
+      this.movies = [];
+      return;
+    }
+
+    this.searching = true;
     
+    // Search Movie
     this.moviesService.searchMovie(movie)
       .subscribe(response => {
-        console.log(response);
+        this.movies = response['results'];
+        this.searching = false;
       });
   }
 
