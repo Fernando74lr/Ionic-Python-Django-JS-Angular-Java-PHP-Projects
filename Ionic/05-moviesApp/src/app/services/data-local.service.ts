f1eb22f9e02233e9d28fe6b8d6d4f9741754536e
unknown <flopezramirez@hotmail.com>
2020-10-09 21:50:11 -0500

    Prevent Duplicates In Movie Storage

--- Ionic/05-moviesApp/src/app/services/data-local.service.ts
+++ Ionic/05-moviesApp/src/app/services/data-local.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { MovieDetail } from '../interfaces/interfaces';
 
@@ -9,11 +10,39 @@ export class DataLocalService {
 
   movies: MovieDetail[] = [];
 
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage,
+              private toastController: ToastController) { }
 
   saveMovie(movie: MovieDetail) {
-    this.movies.push(movie);
+    let exists = false;
+    let message = '';
+
+    for (let movieArray of this.movies) {
+      if (movieArray.id === movie.id) {
+        exists = true;
+        break;
+      }
+    }
+
+    if (exists) {
+      this.movies = this.movies.filter(film => film.id !== movie.id);
+      message = 'Removed from favorites';
+    } else {
+      this.movies.push(movie);
+      message = 'Added to favorites';
+    }
+
+    this.presentToast(message);
     this.storage.set('movies', this.movies);
+  }
+
+  // Toast
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
   }
 
 
