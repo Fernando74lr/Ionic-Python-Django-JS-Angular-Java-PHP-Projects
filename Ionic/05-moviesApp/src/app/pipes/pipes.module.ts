2e86eb3103ca4e32c1298254db770f9823ccf743
unknown <flopezramirez@hotmail.com>
2020-10-11 02:29:23 -0500

    Ios Details

--- Ionic/05-moviesApp/src/app/pipes/pipes.module.ts
+++ Ionic/05-moviesApp/src/app/pipes/pipes.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImagePipe } from './image.pipe';
 import { EvensPipe } from './evens.pipe';
+import { FilterImagePipe } from './filter-image.pipe';
 
 @NgModule({
-  declarations: [ImagePipe, EvensPipe],
+  declarations: [ImagePipe, EvensPipe, FilterImagePipe],
   imports: [
     CommonModule
   ],
   exports: [
     ImagePipe,
-    EvensPipe
+    EvensPipe,
+    FilterImagePipe
   ]
 })
 export class PipesModule { }
