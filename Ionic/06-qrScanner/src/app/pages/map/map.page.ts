294dda06cf2f19592b9e7a2e9324e84330887bc3
unknown <flopezramirez@hotmail.com>
2020-10-27 14:42:13 -0600

    Show Map With The Coordinates Given

--- Ionic/06-qrScanner/src/app/pages/map/map.page.ts
+++ Ionic/06-qrScanner/src/app/pages/map/map.page.ts
@@ -1,30 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+declare var mapboxgl: any;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
-export class MapPage implements OnInit {
+export class MapPage implements OnInit, AfterViewInit {
 
   lat: number;
   lng: number;
 
   constructor(private route: ActivatedRoute) { }
-
+  
   ngOnInit() {
     let geo: any = this.route.snapshot.paramMap.get('geo');
-
-    console.log(geo.substring(4));
 
     geo = geo.substring(4);
     geo = geo.split(',');
 
     this.lat = Number(geo[0]);
     this.lng = Number(geo[1]);
-
+    
     console.log(this.lat, this.lng)
+  }
+
+  ngAfterViewInit() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZmVybmFuZG83NGxyIiwiYSI6ImNrZ3NlZGRucDAwZnMycW4yeDgwNDZ6ZnoifQ.WBETnEn0ibpMNFksLJfHbA';
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
   }
 
 }
