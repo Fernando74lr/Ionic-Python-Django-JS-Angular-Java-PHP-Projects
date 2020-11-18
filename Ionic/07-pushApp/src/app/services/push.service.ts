69cb357f6214e638db69759bd5e5057d0141d36c
unknown <flopezramirez@hotmail.com>
2020-11-18 15:40:16 -0600

    Showing Received Notifications In Our App

--- Ionic/07-pushApp/src/app/services/push.service.ts
+++ Ionic/07-pushApp/src/app/services/push.service.ts
@@ -1,30 +1,48 @@
 import { Injectable } from '@angular/core';
-import { OneSignal } from '@ionic-native/onesignal/ngx';
+import { OneSignal, OSNotification } from '@ionic-native/onesignal/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PushService {
 
+  messages: any[] = [
+    {
+      title: 'Title push',
+      body: 'This is the body of the push',
+      date: new Date()
+    }
+  ];
+
   constructor(private oneSignal: OneSignal) { }
 
   initialConfiguration() {
     this.oneSignal.startInit('7a3ca103-3377-43e4-aefd-80b7c4148d22', '1016671983209');
 
-    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
 
-    this.oneSignal.handleNotificationReceived().subscribe((noti) => {
     // Do something when notification is received
-    console.log('Notification received', noti);
-    
+    this.oneSignal.handleNotificationReceived().subscribe((noti) => {
+      console.log('Notification received', noti);
+      this.receivedNotification(noti);
     });
 
+    // Do something when a notification is opened
     this.oneSignal.handleNotificationOpened().subscribe((noti) => {
-      // Do something when a notification is opened
       console.log('Notification opened', noti);
-
     });
 
     this.oneSignal.endInit();
+  }
+
+  receivedNotification(noti: OSNotification) {
+    const payload = noti.payload;
+    const existsPush = this.messages.find(message => message.notificationID === payload.notificationID);
+
+    if (existsPush) {
+      return;
+    }
+
+    this.messages.unshift(payload);
   }
 }
