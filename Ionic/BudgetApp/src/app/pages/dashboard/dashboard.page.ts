1eaf5c2968a93a390d186b803515bb358ac6ed8e
Fernando74lr <flopezramirez@hotmail.com>
2020-12-17 18:18:27 -0600

    Inital Configurations (Composer Installation)

--- Ionic/BudgetApp/src/app/pages/dashboard/dashboard.page.ts
+++ Ionic/BudgetApp/src/app/pages/dashboard/dashboard.page.ts
@@ -65,6 +65,10 @@ export class DashboardPage implements OnInit {
                     // fontColor: "#ffffff"
                 }
             }]
+        },
+        animation: {
+          duration: 750,
+          easing: 'easeInQuad'
         }
     }
     });
