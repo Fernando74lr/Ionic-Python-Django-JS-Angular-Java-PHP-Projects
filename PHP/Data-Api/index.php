6b1c200e1ba5e7ec89a118bf774ba438884900a3
Fernando74lr <flopezramirez@hotmail.com>
2020-12-27 23:55:49 -0600

    Send Request Throught Curl

--- PHP/Data-Api/index.php
+++ PHP/Data-Api/index.php
@@ -4,5 +4,18 @@
 
     $request = "https://labopat-5792e.appspot.com/getHoras?day=" + $date;
 
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $request,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "cache-control: no-cache"
+        ),
+    ));
+
     
 ?>
