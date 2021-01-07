630577faa2e73208cf801d07ffe160cbdc6bd942
Fernando74lr <flopezramirez@hotmail.com>
2021-01-07 01:42:39 -0600

    Fixed Connection

--- Python/Django/Connect-SQLServer/sqlServerConnect/sqlServerConnect/views.py
+++ Python/Django/Connect-SQLServer/sqlServerConnect/sqlServerConnect/views.py
@@ -3,11 +3,16 @@ from sqlServerConnect.models import sqlServerConn
 import pyodbc
 
 def connSql(request):
-    conn = pyodbc.connect('Driver={sql server};'
-                          'Server=DESKTOP-60ILFM6\COMPAC;'
-                          'Database=adCOM_GRUPO_SANTA_MARIA_SA;'
-                          'Trusted_Connection=yes;')
+    server = 'DESKTOP-60ILFM6\\COMPAC' 
+    database = 'adCOM_GRUPO_SANTA_MARIA_SA' 
+    username = 'sa' 
+    password = 'Contpaqi1$' 
+    conn = pyodbc.connect('DRIVER={sql server};'+
+                          'SERVER=' + server + ';'+
+                          'DATABASE=' + database + ';'+
+                          'UID=' + username + ';'+
+                          'PWD=' + password + ';')
     cursor = conn.cursor()
-    cursor.execute("SELECT CIDCLIENTEPROVEEDOR CRAZONSOCIAL CRFC FROM admClientes")
+    cursor.execute("SELECT CIDCLIENTEPROVEEDOR, CRAZONSOCIAL, CRFC FROM admClientes")
     result = cursor.fetchall()
     return render(request, 'index.html', {'sqlServerConn' : result})
