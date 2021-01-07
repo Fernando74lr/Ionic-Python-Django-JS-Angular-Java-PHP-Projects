c6be074c0e475fb921472ff9c008e2e9a1243ed7
Fernando74lr <flopezramirez@hotmail.com>
2021-01-07 01:54:58 -0600

    Get Data From Multiple DBs (Enterprises)

--- Python/Django/Connect-SQLServer/sqlServerConnect/sqlServerConnect/views.py
+++ Python/Django/Connect-SQLServer/sqlServerConnect/sqlServerConnect/views.py
@@ -3,16 +3,28 @@ from sqlServerConnect.models import sqlServerConn
 import pyodbc
 
 def connSql(request):
+    enterprises = [
+        'adCOM_GRUPO_SANTA_MARIA_SA',
+        'adCOM_KL_13_SERVICIOS_SA',
+        'adCOM_YUNESGO_ASOCIADOS_S'
+    ]
+    
     server = 'DESKTOP-60ILFM6\\COMPAC' 
-    database = 'adCOM_GRUPO_SANTA_MARIA_SA' 
+    # database = 'adCOM_GRUPO_SANTA_MARIA_SA' 
     username = 'sa' 
-    password = 'Contpaqi1$' 
-    conn = pyodbc.connect('DRIVER={sql server};'+
-                          'SERVER=' + server + ';'+
-                          'DATABASE=' + database + ';'+
-                          'UID=' + username + ';'+
-                          'PWD=' + password + ';')
-    cursor = conn.cursor()
-    cursor.execute("SELECT CIDCLIENTEPROVEEDOR, CRAZONSOCIAL, CRFC FROM admClientes")
-    result = cursor.fetchall()
+    password = 'Contpaqi1$'
+
+    result = []
+
+    for database in enterprises:
+        conn = pyodbc.connect('DRIVER={sql server};'+
+                            'SERVER=' + server + ';'+
+                            'DATABASE=' + database + ';'+
+                            'UID=' + username + ';'+
+                            'PWD=' + password + ';')
+        cursor = conn.cursor()
+        cursor.execute("SELECT CIDCLIENTEPROVEEDOR, CRAZONSOCIAL, CRFC FROM admClientes")
+        result.append(cursor.fetchall())
+
+
     return render(request, 'index.html', {'sqlServerConn' : result})
