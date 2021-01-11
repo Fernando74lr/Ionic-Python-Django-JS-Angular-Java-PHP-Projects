491ae6b3b4abdf7200dc8d45999b74b3f65283d5
Fernando74lr <flopezramirez@hotmail.com>
2021-01-10 18:06:21 -0600

    Static Content and Bootstrap Template

--- Python/Django/Reports/core/views.py
+++ Python/Django/Reports/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'core/index.html')
+
+def reports(request):
+    return render(request, 'core/reports.html')
