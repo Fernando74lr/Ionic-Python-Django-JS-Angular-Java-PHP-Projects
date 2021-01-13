74565f9326d22da1b8bee860e5d84e648c58dae3
Fernando74lr <flopezramirez@hotmail.com>
2021-01-12 23:57:48 -0600

    Modifying Views

--- Python/Django/Reports/report/views.py
+++ Python/Django/Reports/report/views.py
@@ -1,6 +1,9 @@
+from .data.secret.credentials import server, username, password
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from .models import SqlServerConn
-from .data.secret.credentials import server, username, password
+from openpyxl import Workbook
+import datetime
 import pyodbc
 
 query_cache = []
@@ -53,3 +56,56 @@ def get_clients(request):
 
     # return result
     return render(request, 'report/reports.html', {'SqlServerConn' : result})
+
+def clients_report(request):
+    wb = Workbook()
+    ws = wb.active
+    ws['A1'] = 'REPORTE CLIENTES'
+    ws.merge_cells('A1:B1')
+    ws['C1'] = datetime.datetime.now()
+
+    # DATA
+    ws['A3'] = 'ID CLIENTE'
+    ws['B3'] = 'CORPORACIÓN'
+    ws['C3'] = 'RAZÓN SOCIAL'
+    ws['D3'] = 'RFC'
+    ws['E3'] = 'ID AGENTE VENTA'
+    ws['F3'] = 'FECHA ALTA'
+    ws['G3'] = 'ESTATUS'
+    ws['H3'] = 'BASE DE DATOS'
+
+    global query_cache
+    counter = 4
+
+    if len(query_cache) > 0:
+        print("\nCREATING EXCEL\n")
+        for enterprise in query_cache:
+            for data in enterprise:
+                # ID Cliente
+                ws.cell(row=counter,column=1).value = data[2]
+                # Corporación
+                ws.cell(row=counter,column=2).value = data[1]
+                # Razón Social
+                ws.cell(row=counter,column=3).value = data[3]
+                # RFC
+                ws.cell(row=counter,column=4).value = data[4]
+                # ID Agente Venta
+                ws.cell(row=counter,column=5).value = data[7]
+                # Fecha Alta
+                ws.cell(row=counter,column=6).value = str(data[5]).split(' ')[0]
+                # Estatus
+                ws.cell(row=counter,column=7).value = data[6]
+                # Base de datos
+                ws.cell(row=counter,column=8).value = data[0]
+
+                counter+=1
+    else:
+        print("\nCACHE EMPTY\n")
+
+    filename = 'Reporte_Clientes.xlsx'
+    response = HttpResponse(content_type = 'application/ms-excel')
+    content = 'attachment; filename = {0}'.format(filename)
+    response['Content-Disposition'] = content
+    wb.save(response)
+
+    return response
