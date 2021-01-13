f6e8a190e7fab345b3f1cd7ab0dc5ad7c24aa967
Fernando74lr <flopezramirez@hotmail.com>
2021-01-12 23:58:18 -0600

    Urls Django

--- Python/Django/Reports/report/urls.py
+++ Python/Django/Reports/report/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # Report
     path('reports/', views.reports, name='reports'),
-    path('reports/clients', views.get_clients, name='clients')
+    path('reports/clients', views.get_clients, name='clients'),
+    path('reports/clients/results', views.clients_report, name='results'),
 ]
