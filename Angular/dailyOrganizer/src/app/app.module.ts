b01652a7232c7c6bd358657608c96a2e49ebee8a
Fernando74lr <flopezramirez@hotmail.com>
2021-01-25 23:22:37 -0600

    App Module

--- Angular/dailyOrganizer/src/app/app.module.ts
+++ Angular/dailyOrganizer/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { TestComponent } from './pages/test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    TestComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
