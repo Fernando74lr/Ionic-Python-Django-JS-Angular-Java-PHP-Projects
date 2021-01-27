041efabc65d9642d58c48fcdd4c575acd63914ff
Fernando74lr <flopezramirez@hotmail.com>
2021-01-27 01:06:29 -0600

    Finished Functional Daily Optimizer Habit Tracker (Github Contributions)

--- JavaScript/Daily_Optimizer/Habit_Tracker/habit_tracker.js
+++ JavaScript/Daily_Optimizer/Habit_Tracker/habit_tracker.js
@@ -19,9 +19,17 @@ let contribution = (i, date, month, year, count) => {
 
         </div>
     `);   
+};
+
+let weekDayDiv = (i, count, weekDay) => $(`#contributions-${i}`).append(`<div class="weekDate" id="date-${count}">${weekDay}</div>`);
+
+function getDayDate(date, month, year) {
+    new_date = new Date(`${year}-${month}-${date}`);
+    console.log(new_date);
+    return new_date.toString().split(' ')[0];
 }
 
-function getSundayDate(date, year, month) {
+function getSundayDate(date, month, year) {
     let day = '', new_date = '';
     month++;
     while (day != 'Sun') {
@@ -35,7 +43,6 @@ function getSundayDate(date, year, month) {
                 month--;
             } else {
                 month = 12;
-                // year--;
             }
             date = MONTHS[month - 1];
         }
@@ -43,38 +50,85 @@ function getSundayDate(date, year, month) {
     return parseInt(new_date.toString().split(' ')[2]) - 1;
 }
 
+function getNumberMonths() {
+    res = [];
+    n = -7;
+    for (let i = 0; i < 53; i++) res.push(n+=8);
+    return res;
+}
+
 function printContributions() {
-    let count = 0;
+    let nextMonth = '';
+    let newRow = 54;
+    let count = -7;
+    let weekDay = '';
+    let drawedMonths = [];
     let day = TODAY.getDay();
     let date = TODAY.getDate();
     let month = TODAY.getMonth();
     let year = TODAY.getFullYear()-1;
-    let sunDate = getSundayDate(date, year, month) + (day == 0 ? 7 : 0);
+    const NUMBER_MONTHS = getNumberMonths();
+    let sunDate = getSundayDate(date, month, year) + (day == 0 ? 7 : 0);
     if (sunDate > DAY_MONTHS[month]) { 
         sunDate -= DAY_MONTHS[month];
     }
 
-    for (let i = 0; i < 53; i++) {
+    for (let i = 0; i < 54; i++) {
         container(i);
-        for (let j = 0; j < 7; j++) {
+        for (let j = 0; j < 8; j++) {
             // debugger;
-            if (count != (365 + day)) { 
-                if (sunDate != DAY_MONTHS[month]) {
-                    // New day
-                    sunDate++;
-                } else {
-                    // New month
-                    sunDate = 1;
-                    if (month + 1 == 12) {
-                        // New Year
-                        month = 0;
-                        year++;  
-                    } else {
-                        month++;
-                    }
+            if (count < 1) {
+                switch (count) {
+                    case -5: weekDay = 'Mon'; break;
+                    case -3: weekDay = 'Wed'; break;
+                    case -1: weekDay = 'Fri'; break;
+                    default: weekDay = ''; break;
                 }
                 count++;
-                contribution(i, sunDate, month, year, count);
+                weekDayDiv(i, count, weekDay);
+            } else {
+                if (NUMBER_MONTHS.includes(count)) {
+                    if (sunDate == DAY_MONTHS[month]) {
+                        if (!drawedMonths.includes(MONTHS[month])) {
+                            nextMonth = MONTHS[month+1];
+                            drawedMonths.push(nextMonth);
+                        }
+                        weekDayDiv(i, count, `<span style="padding-left:10px">${nextMonth}</span>`);
+                        nextMonth = '';
+                    } else {
+                        if (nextMonth.length > 0) {
+                            weekDayDiv(i, count, `<span style="padding-left:10px">${nextMonth}</span>`);
+                            nextMonth = '';
+                        } else {
+                            weekDayDiv(i, count, '');
+                        }
+                    }
+                    count++;
+                } else {
+                    if (count != (365 + newRow + day)) {
+                        if (sunDate != DAY_MONTHS[month]) {
+                            // New day
+                            sunDate++;
+                        } else {
+                            // New month
+                            if (month + 1 == 12) {
+                                // New Year
+                                month = 0;
+                                year++;  
+                            } else {
+                                month++;
+                            }
+                        
+                            sunDate = 1;
+                            if (!drawedMonths.includes(MONTHS[month])) {
+                                nextMonth = MONTHS[month];
+                                drawedMonths.push(nextMonth);
+                            }
+                        }
+                        count++;
+                        contribution(i, sunDate, month, year, count);
+                    }
+                }
             }
         }
     }
