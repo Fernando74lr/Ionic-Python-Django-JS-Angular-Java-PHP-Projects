9d0da80ce017169dc4a04678f4a19739797c8ab4
Fernando74lr <flopezramirez@hotmail.com>
2021-02-13 23:57:24 -0600

    Update Firebase.js

--- JavaScript/Kanban_Firebase/firebase.js
+++ JavaScript/Kanban_Firebase/firebase.js
@@ -1,46 +1,77 @@
 
-// var admin = require("firebase-admin");
-
-// var serviceAccount = require("secret/apiKey/simple-kanban-board-firebase-adminsdk-gu08s-3d2e8866a6.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://simple-kanban-board-default-rtdb.firebaseio.com"
-// });
-
-// Firebase Credentials
-var config = {
-    apiKey: "3d2e8866a6209a9972b4cf5dec3ca1f08b4298c3",
-    authDomain: "simple-kanban-board.firebaseapp.com",
-    databaseURL: "https://simple-kanban-board-default-rtdb.firebaseio.com/",
-    storageBucket: "simple-kanban-board.bucket.appspot.com"
+const config = {
+    apiKey: 'AIzaSyACs3O_QpP_mYUiievS5lJa1Cs8oE3s-QQ',
+    authDomain: "tests-b5ca7.firebaseapp.com",
+    databaseURL: "https://tests-b5ca7.firebaseio.com/",
+    projectId: "tests-b5ca7",
+    storageBucket: "tests-b5ca7.appspot.com"
 };
 
-// Initialize App
 firebase.initializeApp(config);
+const firestore = firebase.firestore();
 
-// Get a reference to the database service
-var database = firebase.database().ref().child('elements');
+// Get elements
+// const btnSubmitCard = $('#add-card');
+let email = $('#txtEmail').val();
+let password = $('#txtPassword').val();
+const btnLogin = $('#btnLogin');
+const btnLogout = $('#btnLogout');
+const btnSignUp = $('#btnSignUp');
 
-database.on('value',function(snapshot){
+btnLogout.click(e => {
+    let name = $('#txtName').val();
+    firestore.doc(`users/${name}`).get()
+        .then(doc => {
+            if (doc && doc.exists) {
+                const myData = doc.data();
+                $('#name').html(myData.name);
+                $('#email').html(myData.email);
+            }
+        })
+});
 
-    snapshot.forEach(function(childSnapshot) {
+btnLogin.click(e => {
+    let name = $('#txtName').val();
+    // Store
+    firestore.doc(`users/${name}`).set({
+        name: name,
+        email: email,
+        password: password
+    }).then(() => {
+        console.log('Everything alright')
+    }).catch((error) => {
+        console.log('Got an error', error);
+    });
+});
 
-        console.log(childSnapshot.key)
+// Add a new card
+function addCard(id, text) {
+    if ($('#add-card')) {
+        $('#add-card').click(e => {
+            firestore.doc(`users/Fernando López Ramírez/tasks/${id}`).set({
+                description: text,
+                date_created: getCurrentDate(),
+                time_created: getCurrentTime()
+            }).then(() => {
+                console.log('Task created')
+            }).catch((error) => {
+                console.log('Got an error adding the task', error);
+            });
+        });
+    }
+}
 
-        console.log(childSnapshot.val())
-    })
 
-})
-
-// function writeElementData(elementId, text, date_created, time_created) {
-//     database.ref('elements/element' + elementId).set({
-//         id: elementId,
-//         text: text,
-//         date_created: date_created,
-//         time_created: time_created
+// Add data
+// function storeUser(name, email) {
+//     db.collection("users").add({
+//         name: name,
+//         email: email,
+//     })
+//     .then(function(docRef) {
+//         console.log("Document written with ID: ", docRef.id);
+//     })
+//     .catch(function(error) {
+//         console.error("Error adding document: ", error);
 //     });
-//     console.log('ELEMENT CREATED');
 // }
-
-// writeElementData('1', 'This is a test', '08-02-2021', '23:23');
