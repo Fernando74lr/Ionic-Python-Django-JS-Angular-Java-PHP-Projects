1c8f46ff31cd52e798505b9639a2d4725f86d431
Fernando López Ramírez <39963559+Fernando74lr@users.noreply.github.com>
2021-05-16 23:01:07 -0500

    Update Convert string_to_camel_case.py

--- CodeWars/Python/Convert string_to_camel_case.py
+++ CodeWars/Python/Convert string_to_camel_case.py
@@ -4,4 +4,5 @@ def to_camel_case(text):
     res = []
     for word in text.split('-')[1:len(text)+1]:
         res.append(word.capitalize())  
+        
     return text.split('-')[0] + ''.join(res)
