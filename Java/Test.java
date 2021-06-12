8a703f20fa15baae5ba7498fe830a870c1b2ba83
Fernando López Ramírez <39963559+Fernando74lr@users.noreply.github.com>
2021-06-11 23:58:27 -0500

    Update Test.java

--- Java/Test.java
+++ Java/Test.java
@@ -8,7 +8,6 @@ public class Test {
     public static void main(String[] args) {
         // Get values
        String [] res = getValues("32,45,24,13");
-       
        // Print result
        for(int i=0; i<res.length; i++) {
           System.out.println(res[i]);
